ef8ad13df64eb525c1ba4cbc990599e8fa4f65c0
nguyenhuutruong123 <71997835+nguyenhuutruong123@users.noreply.github.com>
2022-06-22 21:59:46 +0700

    Update forminpdf.java

--- src/demo/dcl/form/forminpdf.java
+++ src/demo/dcl/form/forminpdf.java
@@ -35,7 +35,7 @@ public class forminpdf extends HttpServlet {
 	    response.setCharacterEncoding("UTF-8");
 	    response.setContentType("text/html; charset=UTF-8"); 	    
 	    HttpSession session = request.getSession();	    
-	    String nextJSP = request.getContextPath() + "/pages/Center/formin.jsp";
+	    String nextJSP = request.getContextPath() + "/pages/Center/formin1.jsp";
 		response.sendRedirect(nextJSP);
 		
 	}
